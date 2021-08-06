31d0fbe331c7eeefe88a2c20919de32b5ebe55ed
nandiniawtani11 <62164797+nandiniawtani11@users.noreply.github.com>
2021-08-06 16:13:09 +0530

    Create LeetCode-Is-Subsequence.java

---
+++ LeetCode-Is-Subsequence.java
@@ -0,0 +1,22 @@
+class Solution {
+    public boolean isSubsequence(String s, String t) {
+        int i=0;
+        int j=0;
+        while(i<s.length() && j<t.length())
+        {
+            if(s.charAt(i)==t.charAt(j))
+            {
+                i++;
+                j++;
+            }
+            else
+            {
+                j++;
+            }
+        }
+        if(j==t.length()&&i!=s.length())
+            return false;
+        else
+            return true;
+    }
+}
